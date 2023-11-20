54c62148d36d4cf69644e0506d3b1cdc09617aa1
Arghun Mousanezhad <53907570+Arghun-dev@users.noreply.github.com>
2023-11-20 09:30:53 +0100

    Update api.ts
    Why I didn't handle server side pagination

--- lib/api.ts
+++ lib/api.ts
@@ -10,6 +10,7 @@ type CertificatesApiResponse = {
 
 export async function getCertificates(): Promise<CertificatesApiResponse> {
 	try {
+		// I have intentionally requested to get all certificates and didn't handle server side pagination and limit, because I noticed only 12 records exist in the database, but the correct way is to handle pagination server side.
 		const res = await fetch(`${process.env.BASE_API}/certificates`, {
 			headers: {
 				'API-ACCESS-TOKEN': process.env.API_ACCESS_TOKEN || '',
